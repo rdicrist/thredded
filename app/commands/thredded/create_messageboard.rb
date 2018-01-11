c75fd9faedda32d3baf574b4da8e50b823ee13e0
Billy Groble <BillG18@users.noreply.github.com>
2018-01-11 17:45:57 -0500

    updated default first post

--- app/commands/thredded/create_messageboard.rb
+++ app/commands/thredded/create_messageboard.rb
@@ -36,13 +36,13 @@ module Thredded
     def first_post_content
       <<-MARKDOWN
 **We take moderation very seriously so make sure you're familiar with the rules before you post to make sure your posts don't get removed!**
-    
-    1. DO NOT identify yourself in any way
-    2. If you suspect you know someone on the forum, DO NOT identify them in any way
-    3. DO NOT use any information to find out real world facts about anyone on the forum
-    4. Be kind, courteous, and respectful
-    5. If any personally identifiable information (such as names, emails, phone numbers, etc.) is posted, it will be removed from the site as soon as possible.
-    6. Any members that are disrespectful, inappropriate, or aggressive will be blocked.
+
+1. DO NOT identify yourself in any way
+2. If you suspect you know someone on the forum, DO NOT identify them in any way
+3. DO NOT use any information to find out real world facts about anyone on the forum
+4. Be kind, courteous, and respectful
+5. If any personally identifiable information (such as names, emails, phone numbers, etc.) is posted, it will be removed from the site as soon as possible.
+6. Any members that are disrespectful, inappropriate, or aggressive will be blocked.
     MARKDOWN
     end
   end
